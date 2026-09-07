3f0367a2b29ddf10a033dba679286dbea5cc5e5c
Meekal-Jiwani <156714624+Meekal-Jiwani@users.noreply.github.com>
2026-09-06 22:03:29 -0700

    Integrate logger and update console messages

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const net = require('net');
 
+const logger = require("./logger").default("server");
+
 const server = net.createServer();
 const port = 6379;
 const host = '127.0.0.1';
 
 
 server.on("connection", (socket) => {
-    console.log("Client connected");
+    console.log("Client Connected");
 
     socket.on("data", (data) => {
         const reqData = data.toString();
+        logger.log(reqData);
         
         socket.write("res: " + reqData);
     });
 
     socket.on("end", () => {
-        console.log("Client disconnected");
+        console.log("Client Disconnected");
     });
 });
 
 server.listen(port, host, () => {
-    console.log(`Server is running on ${host}:${port}`);
+    logger.log(`Server is running on ${host}:${port}`);
 });
